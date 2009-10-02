10b89c838790755a4b0d71703a54deb8f1f9edaf
AviG <ialoneambest@gmail.com>
2009-10-02 10:50:24 +0100

    Everything Again!

--- live/bs-navision-backup.sh
+++ live/bs-navision-backup.sh
@@ -3,9 +3,26 @@
 STAMP=$(date +%Y-%m-%d-%H%M)
 FROM_DIR="/mnt/glasgow/navision/"
 TO_DIR="/mnt/london/bs/Navision/"$STAMP
-MD5_FILE=$TO_DIR/database.fdb.md5
+TEMP_DIR="/mnt/london/bs/Navision/"
+
 
 mkdir $TO_DIR
 
-#rsync -a -vv $FROM_DIR* $TO_DIR | mail -a "From: BS Backup" -s "BS Backup $date" avi.greenbury@servicegraphics.co.uk
-cp $FROM_DIR* $TO_DIR
+fs_remote=$(ls -l $FROM_DIR/database.fdb | awk '{print $5}')
+rsync -a --no-whole-file -vv $FROM_DIR* $TEMP_DIR
+fs_local=$(ls -l $TEMP_DIR/database.fdb | awk '{print $5}') 
+
+cp $TEMP_DIR $TO_DIR
+
+if [$fs_remote != $fs_local]; then
+	echo "File seems to have changed size in transit:"
+	echo "	Remote:	$fs_remote b"
+	echo "	Local:	$fs_local b"
+fi
+
+
+echo $STAMP $fs_remote $fs_local >> ./bs-log
+
+#cp $FROM_DIR* $TO_DIR
+
+
