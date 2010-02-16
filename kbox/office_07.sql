a04f17a11970b4cc744ae6fca166112095d278d4
AviG <ialoneambest@gmail.com>
2010-02-16 15:48:42 +0000

    added comments, made it actually work (now returns expected resultset)

--- kbox/office_07.sql
+++ kbox/office_07.sql
@@ -1,6 +1,17 @@
--- SQL behind machines-with-office-07-on-them report:
+-- SQL behind machines-with-office-07-on-them report --
 
-select MACHINE.NAME, SOFTWARE.DISPLAY_NAME
+/*
+First joins the M_S_JT with MACHINES to add machine data to each
+software install. Left joins this with SOFTWARE to add software info
+to each install. We now have a table of every software install, 
+detailling the machine and software installed.
+
+Then joins the above with LABEL so we can GROUP BY label to make the
+kbox output pretty. This isn't implemented yet, since I've not configured
+the labels on the kbox.
+*/
+
+select MACHINE.NAME, SOFTWARE.DISPLAY_NAME, LABEL.DISPLAY_NAME
 
 from (
 	
@@ -8,16 +19,18 @@ from (
 		(
 			MACHINE_SOFTWARE_JT join MACHINE on
 				MACHINE_SOFTWARE_JT.MACHINE_ID = MACHINE.ID
-		) join (
+		) left join (
 			SOFTWARE
 		) on MACHINE_SOFTWARE_JT.SOFTWARE_ID = SOFTWARE.ID
 	
-	) join (
+	) left join (
 		MACHINE_LABEL_JT join LABEL on
 			MACHINE_LABEL_JT.LABEL_ID=LABEL.ID
 	) on MACHINE_LABEL_JT.MACHINE_ID = MACHINE.ID
 			
 )
 
-WHERE SOFTWARE.DISPLAY_NAME
-REGEXP '^Microsoft Office[ ]*[a-z]*[ ]2007$'
+where SOFTWARE.DISPLAY_NAME
+regexp '^Microsoft Office[ ]*[a-z]*[ ]2007$'
+
+
